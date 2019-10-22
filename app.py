c4608a46b34ce66856a1726d9f7f67c9468981ba
Megan <megangeisreiter@gmail.com>
2019-10-22 18:45:55 -0400

    further updates

--- app.py
+++ app.py
@@ -10,33 +10,54 @@ import pickle
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.title='DC Crime'
+app.title='DC Crime 🚨'
 
 ########## Define the data
-df = pd.read_csv('resources/crime_shortdf.csv')
-##df=df[df['PRICE'].between(300000, 500000)] # artificially reduce the number of data points for efficiency
+df = pd.read_csv('resources/crime_incidents_new.csv')
+
+###### Set up variables
+list_of_choices=['Homicide', 'Robbery', 'Motor Vehicle Theft', 'Burglary', 'Arson']
 
 ########## Define the figure
 
-fig = go.Figure(go.Densitymapbox(lat=df['LATITUDE'], lon=df['LONGITUDE'], z=df['COUNT'], radius=10))
-fig.update_layout(mapbox_style="stamen-terrain",
-                  mapbox_center_lon=-77.07,
-                  mapbox_center_lat=38.92,
-                  mapbox_zoom=11,
-                 )
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+def generateFigure(offense):
+    df_filtered = df[df['OFFENSE'] == offense.upper()]
+    fig = go.Figure(go.Densitymapbox(lat=df_filtered['LATITUDE'], lon=df_filtered['LONGITUDE'], z=df_filtered['Count'], radius=10))
+    fig.update_layout(mapbox_style="stamen-terrain",
+                    mapbox_center_lon=-77.07,
+                    mapbox_center_lat=38.92,
+                    mapbox_zoom=11,
+                    )
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    return fig
+
 
 ########### Set up the layout
 
 app.layout = html.Div(children=[
     html.H1('Crime in DC'),
+    html.H3('Select an Offense'),
+    html.Br(),
+    dcc.Dropdown(id='your-input-here',
+            options=[{'label': i, 'value': i} for i in list_of_choices],
+            value='Homicide',
+            style={'width': '500px'}),
+    html.Br(),
     html.Div([
-        dcc.Graph(id='figure-1', figure=fig),
+        dcc.Graph(id='figure-1', figure=generateFigure('OFFENSE')),
         html.A('Code on Github', href='https://github.com/mgeisreiter/dash-density-heatmap'),
         html.Br(),
         html.A('Source:', href='https://plot.ly/python/mapbox-density-heatmaps')
     ])
 ])
+
+
+######### Interactive callbacks go here #########
+@app.callback(dash.dependencies.Output('figure-1', 'figure'),
+              [dash.dependencies.Input('your-input-here', 'value')])
+def updateFig(whatever_you_chose):
+    return generateFigure(whatever_you_chose)
+
 
 
 ############ Execute the app
